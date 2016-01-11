4041798dd3a1b30000216dff4473f18e9c96446a
KohYuXuan <spadar_007@hotmail.com>
2016-01-11 21:51:34 +0800

    Update test.js

--- test.js
+++ test.js
@@ -6,11 +6,15 @@ document.getElementsByTagName('head')[0].appendChild(script);
 
 
 window.onload = function() {
+  /*
   var jqxhr = $.getJSON( "http://pianopick.com/test.php?callback=?", function(data) {
     console.log( "success : ", data );
   })
   .done(function() {
     console.log( "second success" );
-  });
+  });*/
+  $.post( "http://pianopick.com/test.php", function( data ) {
+      console.log( "success : ", data );
+});
 
 };
