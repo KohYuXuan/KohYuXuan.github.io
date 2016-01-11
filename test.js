d527b2e082e1faa91bdbce9bd9e19bf77ec4ed04
KohYuXuan <spadar_007@hotmail.com>
2016-01-11 21:35:00 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
 
-alert("test");
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+window.onload = function() {
+  $.post( "http://pianopick.com/test.php", { name: "John", time: "2pm" })
+    .done(function( data ) {
+      alert( "Data Loaded: " + data );
+    });
+};
