c268779d5c152fa2478942259527976bb6c85db2
KohYuXuan <spadar_007@hotmail.com>
2016-01-11 21:50:14 +0800

    Update test.js

--- test.js
+++ test.js
@@ -4,13 +4,10 @@ script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-function receiver(data) {
-  console.log("receive from server : ", data);
-}
 
 window.onload = function() {
-  var jqxhr = $.getJSON( "http://pianopick.com/test.php?callback=receiver", function() {
-    console.log( "success" );
+  var jqxhr = $.getJSON( "http://pianopick.com/test.php?callback=?", function(data) {
+    console.log( "success : ", data );
   })
   .done(function() {
     console.log( "second success" );
